de7e9498a48993e3b8e847011ee66ab72f4efed8
Shubham-022 <shubhambavistale859@gmail.com>
2026-06-29 03:32:47 +0530

    fixed deployment error

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter} from "react-router";
-import Login from "./features/auth/pages/login"
+import Login from "./features/auth/pages/Login"
 import Register from "./features/auth/pages/Register"
-import Protected from "./features/auth/component/protected"
+import Protected from "./features/auth/component/Protected"
 import Home from "./features/interview/pages/Home"
 import Interview from "./features/interview/pages/Interview"
 
